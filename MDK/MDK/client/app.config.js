075c0231d12126f64dd07b7ffd5e6a3f63c01aab
AmitDebadwar <amit.debadwar@gmail.com>
2016-02-08 03:09:13 +0530

    Login page ui done.

---
+++ MDK/MDK/client/app.config.js
@@ -0,0 +1,13 @@
+﻿
+angular.module('MDKApp').config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $stateProvider.
+      state("login", {
+        url: "/login",
+        templateUrl: "views/login.html",
+      });
+
+  $urlRouterProvider.otherwise("/login");
+
+}]);
+
+
---
+++ MDK/MDK/client/scripts/controller/MainController.js
@@ -0,0 +1,3 @@
+﻿app.controller("MainCtrl", function ($scope) {
+    $scope.msg = "sainath";
+ });
