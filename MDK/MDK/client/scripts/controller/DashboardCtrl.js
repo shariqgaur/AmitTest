15bb5e557ddb5463179973acf14f17cda4e8d1d3
AmitDebadwar <amit.debadwar@gmail.com>
2016-02-10 17:01:43 +0530

    logo added

---
+++ MDK/MDK/client/scripts/controller/DashboardCtrl.js
@@ -0,0 +1,3 @@
+﻿angular.module("MDKApp").controller("DashboardCtrl", ["$scope", function ($scope) {
+    
+ }]);
---
+++ MDK/MDK/client/scripts/controller/LoginCtrl.js
@@ -0,0 +1,10 @@
+﻿angular.module("MDKApp").controller("LoginCtrl",["$scope","$state",function ($scope,$state) {
+    
+
+
+
+    $scope.login = function () {
+        //$scope.isLoginError = true;
+        $state.go("dashboard");
+    };
+ }]);
