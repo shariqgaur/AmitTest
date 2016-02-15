3ec10158504baeec50b9ede657a601b010342b3d
AmitDebadwar <amit.debadwar@gmail.com>
2016-02-15 19:35:11 +0530

    added header controller

---
+++ MDK/MDK/client/scripts/controller/HeaderCtrl.js
@@ -0,0 +1,6 @@
+﻿angular.module("MDKApp").controller("HeaderCtrl", ["$scope", "$state", "apiService", function ($scope, $state, apiService) {
+   
+    $scope.logout = function () {
+        $state.go('login');
+    };
+}]);
