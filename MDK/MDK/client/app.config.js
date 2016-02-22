2d6ee159f9ebcb160d01bb1425d51d1279a2e17a
AmitDebadwar <amit.debadwar@gmail.com>
2016-02-23 00:55:52 +0530

    Added dummy add new user page without UserManagement

--- MDK/MDK/client/app.config.js
+++ MDK/MDK/client/app.config.js
@@ -1,21 +1,26 @@
 ﻿
 angular.module('MDKApp').config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-  $stateProvider.
-      state("login", {
-        url: "/login",
-        templateUrl: "views/login.html",
-        controller: "LoginCtrl"
-      }).state("dashboard", {
-          url: "/dashboard",
-          templateUrl: "views/dashboard.html",
-          controller: "DashboardCtrl"
-      }).state("lineDetails", {
-          url: "/lineDetails",
-          templateUrl:"views/lineDetails.html",
-          controller: "LineDetailsCtrl"
-      });
+    $stateProvider.
+        state("login", {
+            url: "/login",
+            templateUrl: "views/login.html",
+            controller: "LoginCtrl"
+        }).state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "views/dashboard.html",
+            controller: "DashboardCtrl"
+        }).state("lineDetails", {
+            url: "/lineDetails",
+            templateUrl: "views/lineDetails.html",
+            controller: "LineDetailsCtrl"
+        }).state("addUser", {
+            url: "/addUser",
+            templateUrl: "views/userManagement.html",
+            controller: function () { }
 
-  $urlRouterProvider.otherwise("/lineDetails");
+        });
+
+    $urlRouterProvider.otherwise("/addUser");
 
 }]);
 
--- MDK/Services/AdminServices/IAdminServices.cs
+++ MDK/Services/AdminServices/IAdminServices.cs
@@ -1,32 +1,32 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Runtime.Serialization;
-//using System.ServiceModel;
-//using System.ServiceModel.Web;
-//using System.Text;
-//using Models;
-//using Models.TransportModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Models;
+using Models.TransportModel;
 
-//namespace Services.AdminServices
-//{
-//    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAdminServices" in both code and config file together.
-//    [ServiceContract]
-//    public interface IAdminServices
-//    {
-//        [OperationContract]
-//        [WebInvoke(ResponseFormat = WebMessageFormat.Json,
-//            RequestFormat = WebMessageFormat.Json,
-//            BodyStyle = WebMessageBodyStyle.Wrapped,
-//            Method = "POST")]
-//        TLoginData validateUser(string data);
+namespace Services.AdminServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAdminServices" in both code and config file together.
+    [ServiceContract]
+    public interface IAdminServices
+    {
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            Method = "POST")]
+        TLoginData validateUser(string data);
 
 
-//        [OperationContract]
-//        [WebInvoke(ResponseFormat = WebMessageFormat.Json,
-//            RequestFormat = WebMessageFormat.Json,
-//            BodyStyle = WebMessageBodyStyle.Wrapped,
-//            Method = "POST")]
-//        TUserManegementData createUser(string data);
-//    }
-//}
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            Method = "POST")]
+        TUserManagementData createUser(string data);
+    }
+}
